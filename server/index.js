d31a4435cf9c9ce04a74178756ecc545ca8c956e
Koko Kassa <kowserk7@gmail.com>
2019-04-11 01:32:30 -0700

    stopped using ES6 for requiring express

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-// const express = require('express')
+// import express from 'express';
+const express = require('express')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,5 +8,7 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there'});
 })
 
+
+//https://pacific-springs-10248.herokuapp.com/ | https://git.heroku.com/pacific-springs-10248.git
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
